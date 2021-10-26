f1fe54767669033a20ef0a78dd29d67ee3fd0865
Sara <38770404+SaraDrwish@users.noreply.github.com>
2021-10-26 17:09:58 +0300

    zor

---
+++ main.js
@@ -0,0 +1,18 @@
+
+Console.log(repeat(conteiner_flex));
+
+
+function repeat(){
+
+    var conteiner_flex = document.getElementById("conteiner_flex");
+
+    for(var i = 1 ; i >=3 ; i ++){
+
+        i = conteiner_flex ;
+
+    }
+
+
+    return conteiner_flex ;
+}
+
